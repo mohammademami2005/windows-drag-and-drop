06bd6f2997825e8ba50f8af5d2d0f1eb7f5dd77c
{MohammadEmami} <162024353+mohammademami2005@users.noreply.github.com>
2025-08-03 04:56:26 +0330

    added project file and crate component and crate cursor.js and create new custom style for cursor

---
+++ src/component/cursor.js
@@ -0,0 +1,29 @@
+export default function cursor(params) {
+    const bigCircle = document.querySelector("#cursor")
+    const smallCircle = document.querySelector("#sCursor")
+    console.log(bigCircle , smallCircle);
+    
+
+    window.addEventListener("mousemove", (e) => {
+        let left = e.clientX
+        let top = e.clientY
+        bigCircle.style.left = left + "px"
+        bigCircle.style.top = top + "px"
+        smallCircle.style.left = left + "px"
+        smallCircle.style.top = top + "px"
+    })
+
+    window.addEventListener("mousedown" , (e)=>{
+        bigCircle.classList.replace("border-white" , "border-green-400")
+        bigCircle.style.scale = ".7"
+        smallCircle.classList.replace("bg-white" , "bg-black")
+        smallCircle.style.scale = "1.3"
+    })
+    window.addEventListener("mouseup" , (e)=>{
+        bigCircle.classList.replace("border-green-400","border-white" )
+        bigCircle.style.scale = "1"
+        smallCircle.classList.replace("bg-black" ,"bg-white" )
+        smallCircle.style.scale = "1"
+    })
+
+}
